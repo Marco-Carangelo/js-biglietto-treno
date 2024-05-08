f51b8c377db3dddbc20deb96f8b5447375d15f84
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-08 19:50:32 +0200

    feat: user data validation

--- js/main.js
+++ js/main.js
@@ -4,7 +4,16 @@
 
 const userDistance = parseInt(Number(prompt("Inserire la distanza che si vuole percorrere")));
 const userAge = parseInt(Number(prompt("Inserire l'età")));
+let fullPrice = 0;
 
+//Struttura di controllo per verificare che i dati inseriti siano validi
+
+if ((userDistance <= 0 || userAge <= 0) || (isNaN(userDistance) || isNaN(userAge))) {
+
+    alert("Si accettano solo numeri interi positivi per distanza ed età. Ripetere la procedura inserendo valori validi");
+    location.reload();
+
+}
 
 
 //Stampa in console dei risultati
