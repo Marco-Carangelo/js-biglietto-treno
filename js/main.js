6f79d24f039185c85ecb0e8f0b181cbcd2d0835d
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-08 19:36:52 +0200

    add: user assigned variables by prompt

--- js/main.js
+++ js/main.js
@@ -1,2 +1,21 @@
 'use strict';
 
+//Dichiarazione delle variabili
+
+const userDistance = parseInt(Number(prompt("Inserire la distanza che si vuole percorrere")));
+const userAge = parseInt(Number(prompt("Inserire l'età")));
+
+
+
+//Stampa in console dei risultati
+
+console.log(`
+
+********CALCOLO BIGLIETTO TRENO*******
+
+Distanza inserita: ${userDistance}
+Età inserita: ${userAge}
+
+
+
+`)
