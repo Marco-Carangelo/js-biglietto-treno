735af7052cb07dcc49fed1138e4c1008057e4b30
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-08 20:12:40 +0200

    add: discount calculation

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 
 const userDistance = parseInt(Number(prompt("Inserire la distanza che si vuole percorrere")));
 const userAge = parseInt(Number(prompt("Inserire l'età")));
+let fullPrice = 0;
 let price = 0;
 const kmPrice = 0.21;
 
@@ -18,20 +19,39 @@ if ((userDistance <= 0 || userAge <= 0) || (isNaN(userDistance) || isNaN(userAge
 
 //Calcolo del prezzo pieno del biglietto
 
-price = userDistance * kmPrice;
+fullPrice = userDistance * kmPrice;
 
+//Verifica diritto agli sconti
+
+if (userAge < 18) {
+
+    price = fullPrice * 0.8;
+    console.log("L'utente ha diritto ad uno sconto del 20%");
+
+} else {
+    if (userAge > 65) {
+
+        price = fullPrice * 0.6;
+        console.log("L'utente ha diritto ad uno sconto del 40%");
+
+    } else {
+
+        price = fullPrice;
+        console.log("L'utente non ha diritto a sconti");
+
+    }
+
+}
 
 //Stampa in console dei risultati
 
 console.log(`
-
 ********CALCOLO BIGLIETTO TRENO*******
 
 Distanza inserita: ${userDistance}
 Età inserita: ${userAge}
 
-Prezzo pieno: ${price}
+Prezzo pieno: ${fullPrice}
 
-
-
+Prezzo dopo verifica sconti:${price}
 `)
